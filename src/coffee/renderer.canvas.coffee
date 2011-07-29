950b09b97370c58d5031f1f5c152232a55e2a4b6
Ivan Jovanovic <ivan.jovanovic@local.ch>
2011-07-29 12:20:37 +0200

    Version 0.1 done. It evolves when fist generation is manually set

--- src/coffee/renderer.canvas.coffee
+++ src/coffee/renderer.canvas.coffee
@@ -1,6 +1,20 @@
 rs.ji.renderer = {}
 rs.ji.renderer.canvas = {}
 
-rs.ji.renderer.canvas.render = (cgol) ->
+rs.ji.renderer.canvas.render = (canvas, cgol) ->
+  ctx = canvas.getContext "2d";
+  ctx.fillStyle = "rgb(200,0,0)";
+  # will scale to full width
+  box_size = canvas.width / cgol.field_size
+  console.log cgol
+  map = (i, cell) -> 
+    [x, y] = rs.ji.cgol.get_coords i
+    if cell == 0
+      ctx.strokeRect( x*box_size, y*box_size, box_size, box_size )
+    else if cell == 1
+      ctx.fillRect( x*box_size, y*box_size, box_size, box_size )
+
+  (map i, cell for cell, i in rs.ji.cgol.current_generation)
+
 
 
--- src/renderer.canvas.js
+++ src/renderer.canvas.js
@@ -1,5 +1,27 @@
 (function() {
   rs.ji.renderer = {};
   rs.ji.renderer.canvas = {};
-  rs.ji.renderer.canvas.render = function(cgol) {};
+  rs.ji.renderer.canvas.render = function(canvas, cgol) {
+    var box_size, cell, ctx, i, map, _len, _ref, _results;
+    ctx = canvas.getContext("2d");
+    ctx.fillStyle = "rgb(200,0,0)";
+    box_size = canvas.width / cgol.field_size;
+    console.log(cgol);
+    map = function(i, cell) {
+      var x, y, _ref;
+      _ref = rs.ji.cgol.get_coords(i), x = _ref[0], y = _ref[1];
+      if (cell === 0) {
+        return ctx.strokeRect(x * box_size, y * box_size, box_size, box_size);
+      } else if (cell === 1) {
+        return ctx.fillRect(x * box_size, y * box_size, box_size, box_size);
+      }
+    };
+    _ref = rs.ji.cgol.current_generation;
+    _results = [];
+    for (i = 0, _len = _ref.length; i < _len; i++) {
+      cell = _ref[i];
+      _results.push(map(i, cell));
+    }
+    return _results;
+  };
 }).call(this);
